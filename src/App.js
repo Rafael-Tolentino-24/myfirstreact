6b81891c1c01b80113fa938ecc11ea8f502221ed
Rafael-Tolentino-24 <90952799+Rafael-Tolentino-24@users.noreply.github.com>
2021-09-28 19:12:16 +0300

    update2
    update2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. ---rafael
+          Edit <code>src/App.js</code> and save to reload. ---rafael123
         </p>
         <img src="https://media4.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif?cid=ecf05e472iyny52n6hldwvkevb9ms2jtfolfk96a6p0fxark&rid=giphy.gif&ct=g" alt="cat-vibing"></img>
         <a
