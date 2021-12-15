09b1fc12964b79e12f5ca889e6c119f19c190eeb
Rafael-Tolentino-24 <90952799+Rafael-Tolentino-24@users.noreply.github.com>
2021-12-15 15:49:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavBar from './components/NavBar';
 import Router from './components/Router';
-
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Router />
+      <Home />
     </div>
   );
 }
